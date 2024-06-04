d4dd25d0be7d40292542895beb70af48b73fd6bc
mahadi hasan <mahadi.dev@outlook.com>
2024-06-05 01:02:14 +0600

    readd: jwt auth

--- src/utils/jwt-auth.ts
+++ src/utils/jwt-auth.ts
@@ -7,22 +7,22 @@ import { ExtendedRequest } from "../types/extended-request";
 export async function jwtAuthorization(
   req: Request,
   res: Response,
-  next: NextFunction,
+  next: NextFunction
 ) {
   try {
     if (req.url.includes("/bkash/execute-payment/")) {
       return next();
     }
-    //
-    // const bearerToken = await getBearerToken(req);
-    //
-    // const data = jwt.verify(bearerToken, secrets.jwt_secret);
-    // if (!data) {
-    //   throw new Error("Invalid token found");
-    // }
-    //
-    // // save information for later use
-    // setDataInReq(data, req);
+
+    const bearerToken = await getBearerToken(req);
+
+    const data = jwt.verify(bearerToken, secrets.jwt_secret);
+    if (!data) {
+      throw new Error("Invalid token found");
+    }
+
+    // save information for later use
+    setDataInReq(data, req);
     next();
   } catch (error: any) {
     res.status(401).json({
